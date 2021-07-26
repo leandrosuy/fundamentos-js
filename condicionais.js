57d86276fecb35f97c8271d58543308101066b3a
leandrosuy <leandrosuy@gmail.com>
2021-07-26 08:31:35 -0300

    Condicionais

--- condicionais.js
+++ condicionais.js
@@ -1,6 +1,7 @@
 let semafaro = 'verde';
 let mensagem = '';
 
+// IF, ELSE e ELSE IF
 if(semafaro == 'verde') {               // se semafaro for igual a verde
     mensagem = 'Pode passar'            // mensagem recebe 'Pode passar'
 } else if (semafaro == 'amarelo') {     // se não, se semafaro for igual a amarelo
@@ -9,6 +10,23 @@ if(semafaro == 'verde') {               // se semafaro for igual a verde
     mensagem = 'Pare!'                  // mensagem recebe 'Pare!'
 } else {                                // se não
     mensagem = 'Valor inválido'         // mensagem recebe 'Valor inválido'
+}
+
+// SWITCH
+
+switch(semafaro){
+    case 'verde':
+        mensagem = 'Pode passar' 
+    break;
+    case 'amarelo':
+        mensagem = 'Atenção'
+    break;
+    case 'vermelho':
+        mensagem = 'Pare!'
+    break;
+    
+    default: 
+        mensagem = 'Valor inválido'
 }
 
 console.log(mensagem)
