c6cce9406c6ef9af3cf97aadfee389f09e105a82
leandrosuy <leandrosuy@gmail.com>
2021-07-26 08:44:28 -0300

    Operador ternario

---
+++ operadorTernario.js
@@ -0,0 +1,8 @@
+let idade = 15;
+let resultado = idade >= 18 ? 'ok' : 'não ok'
+
+// O Operador ternario tem esse nome pois vai receber três informações
+// a primeira e a condição, a segunda e a ação caso seja verdadeira
+// e a terceira e a ação caso seja falso
+
+console.log(resultado)
