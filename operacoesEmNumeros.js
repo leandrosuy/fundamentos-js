654fc22bf78ed0da64a05633a342612175a6f2d3
leandrosuy <leandrosuy@gmail.com>
2021-07-28 18:46:45 -0300

    Operações em Numeros

---
+++ operacoesEmNumeros.js
@@ -0,0 +1,11 @@
+let numero = 49.91;
+
+let novoNumero = numero.toString(); // transforma em string
+
+let novoNumero = Math.trunc(numero) // Arredonda o numero para baixo
+
+let novoNumero = Math.ceil(numero) // Arredonda o numero para cima
+
+let novoNumero = Math.round(numero) // Arredonda o numero para cima
+
+console.log(novoNumero);
