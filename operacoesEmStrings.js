5f96cd5eb978bf8752a8634b9fff2fd7eded7b1f
leandrosuy <leandrosuy@gmail.com>
2021-07-28 18:38:29 -0300

    Operações em Strings

---
+++ operacoesEmStrings.js
@@ -0,0 +1,17 @@
+let texto = 'https://www.google.com/';
+
+let valor = texto[0]; // Podemos tratar o texto como array e assim pegar uma letra especifica
+
+let tamanho = texto.length; // Podemos pegar o tamanho total do objeto
+
+let substituir = texto.replace('Frase', 'ABC'); // Podemos substituir uma letra ou uma frase inteira
+
+let colocarEmMaisculo = texto.toUpperCase();
+
+let colocarEmMinusculo = texto.toLowerCase();
+
+let removerEspacos = texto.trim();
+
+let transforArray = texto.split('//');
+
+console.log(transforArray);
