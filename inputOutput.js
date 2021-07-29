b3d2caec192812aa009a9aa35e27a213ce5b43e6
leandrosuy <leandrosuy@gmail.com>
2021-07-29 11:44:07 -0300

    Input Output

---
+++ inputOutput.js
@@ -0,0 +1,10 @@
+// alert()          // output
+// console()    // output
+// prompt()         // input
+var nome = prompt("Qual o seu nome ?");
+
+confirm("confirme " + nome);
+
+console.error(nome);
+console.info(nome);
+console.warn(nome);
